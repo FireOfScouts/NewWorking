fe026597ba43337f2bf60be82589146557f40d26
Lars <lvs1998@googlemail.com>
2018-03-22 11:02:00 +0100

    Initial commit

---
+++ Assets/GameBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameBehaviour : NetworkBehaviour {
+
+	public static GameBehaviour gb;
+	List<Player> players;
+
+	void Start () {
+		if (GameBehaviour.gb == null)
+			GameBehaviour.gb = this;
+		else
+			Destroy (this.gameObject);
+		DontDestroyOnLoad (this);
+		players = new List<Player> ();
+	}
+
+	void Update () {
+		Debug.Log ("Amount of players connected: " + players.Count);
+	}
+
+	public void AddPlayer(Player p){
+		players.Add (p);
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+
+	
+	void Start () {
+		if(isLocalPlayer || !isServer)
+			CmdAddPlayer ();
+	}
+
+	[Command]
+	void CmdAddPlayer(){
+		Debug.Log ("Server");
+		GameBehaviour.gb.AddPlayer (this);
+	}
+}
